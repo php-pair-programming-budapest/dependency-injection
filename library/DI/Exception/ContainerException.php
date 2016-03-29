dfc7a41d1b93ab0dc6713e7c8a918ad7f072ef93
Dávid Naményi <david@joszaki.hu>
2016-03-29 19:36:27 +0200

    Added exceptions

---
+++ library/DI/Exception/ContainerException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author David Namenyi <dnamenyi@gmail.com>
+ * Date: 2016.03.29.
+ */
+
+namespace DI\Exception;
+
+
+class ContainerException extends \RuntimeException implements \Interop\Container\Exception\ContainerException {
+
+}
---
+++ library/DI/Exception/NotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author David Namenyi <dnamenyi@gmail.com>
+ * Date: 2016.03.29.
+ */
+
+namespace DI\Exception;
+
+
+class NotFoundException extends \InvalidArgumentException implements \Interop\Container\Exception\NotFoundException {
+
+}
